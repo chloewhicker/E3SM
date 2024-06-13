aa0f5839050246b3cf13c1d5c837143532adb03c
Chloe <cwhicker@umich.edu>
2024-06-13 09:31:57 -0700

    Added new snow Thermal Conductivity in SoilTemperatureMod.F90 based on  Fourteau, et al. (2021) - is now density and temp dependent

--- components/elm/src/main/controlMod.F90
+++ components/elm/src/main/controlMod.F90
@@ -281,7 +281,7 @@ contains
     namelist /elm_inparm/ &
          use_nofire, use_lch4, use_vertsoilc, use_extralakelayers, &
          use_vichydro, use_century_decomp, use_cn, use_crop, use_snicar_frc, &
-         use_snicar_ad, use_extrasnowlayers,&
+         use_snicar_ad, use_extrasnowlayers, use_snow_thk, &
          use_snicar_lndice, use_vancouver, use_mexicocity, use_noio
 
     ! cpl_bypass variables
@@ -723,6 +723,7 @@ contains
     call mpi_bcast (use_vertsoilc, 1, MPI_LOGICAL, 0, mpicom, ier)
     call mpi_bcast (use_extralakelayers, 1, MPI_LOGICAL, 0, mpicom, ier)
     call mpi_bcast (use_extrasnowlayers, 1, MPI_LOGICAL, 0, mpicom, ier)
+    call mpi_bcast (use_snow_thk, 1, MPI_LOGICAL, 0, mpicom, ier)
     call mpi_bcast (use_vichydro, 1, MPI_LOGICAL, 0, mpicom, ier)
     call mpi_bcast (use_century_decomp, 1, MPI_LOGICAL, 0, mpicom, ier)
     call mpi_bcast (use_cn, 1, MPI_LOGICAL, 0, mpicom, ier)
@@ -1024,6 +1025,7 @@ contains
     write(iulog,*) '    use_lake_wat_storage = ', use_lake_wat_storage
     write(iulog,*) '    use_extralakelayers = ', use_extralakelayers
     write(iulog,*) '    use_extrasnowlayers = ', use_extrasnowlayers
+    write(iulog,*) '    use_snow_thk = ', use_snow_thk
     write(iulog,*) '    use_vichydro = ', use_vichydro
     write(iulog,*) '    use_century_decomp = ', use_century_decomp
     write(iulog,*) '    use_cn = ', use_cn
